996ff78f3043cc809c073b45b6d2043a3549eae4
sol <sol.github@gmail.com>
2022-11-06 12:07:48 +0100

    Added Dark/Light mode switch

---
+++ src/components/themeswitch.tsx
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from "react";
+
+import '../styles/styles.css'
+
+export default function ThemeSwitch() {
+    const [isEnabled, setIsEnabled] = useState(true);
+    const [theme, setTheme] = useState<string>('light');
+    const [icon, setIcon] = useState<string>("")
+
+    useEffect(() => {
+        updateTheme(isEnabled);
+    }, [isEnabled]);
+
+    const switchState = () => {
+        setIsEnabled((prevState) => !prevState)
+    }
+
+    const updateTheme = (isEnabled: boolean) => {
+        setTheme(isEnabled ? 'dark' : 'light');
+        setIcon(isEnabled ? "https://www.uplooder.net/img/image/2/addf703a24a12d030968858e0879b11e/moon.svg"
+                               : "https://www.uplooder.net/img/image/55/7aa9993fc291bc170abea048589896cf/sun.svg")
+        document.body.setAttribute('data-theme', theme);
+    }
+
+    return <>
+        <div className="theme-container shadow-dark" onClick={switchState}>
+            <img id="theme-icon" src={icon} alt="ERR"/>
+        </div>
+    </>
+
+}
