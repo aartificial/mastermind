558442e4871c34c3aaf7d441624125dbc6ba3c50
sol <sol.github@gmail.com>
2022-11-06 12:04:16 +0100

    Game logic implemented with user interaction [fixed answer]

---
+++ src/components/mastermind.tsx
@@ -0,0 +1,99 @@
+import {useState} from "react";
+import {GameStatus} from "../contexts/context";
+import ActiveRow from "./activeRow";
+import EmptyRow from "./emptyRow";
+import {useWindow} from "../hooks/useWindow";
+import UsedRow from "./usedRow";
+import Header from "./header";
+import ThemeSwitch from "./themeswitch";
+import Modal from "./modal";
+
+const VALID_KEYS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+const MAX_TURNS = 7
+const NUMBER_LENGTH = 4
+
+export default function Mastermind() {
+
+    const [answer, setAnswer] = useState<string[]>(["1", "2", "3", "4"])
+    const [currentGuess, setCurrentGuess] = useState<string[]>([])
+    const [guesses, setGuesses] = useState<string[][]>([])
+    const [turn, setTurn] = useState<number>(0)
+    const [state, setState] = useState<GameStatus>(GameStatus.PLAYING)
+
+    useWindow('keydown', handleKeyDown)
+
+    function handleKeyDown(event: KeyboardEvent) {
+        const key = event.key
+        onKeyPressed(key)
+    }
+
+    function onKeyPressed(key: string) {
+        if (state !== GameStatus.PLAYING) return
+
+        if (key === 'Backspace' && currentGuess.length > 0){
+            onDelete()
+            return
+        }
+        if (key === 'Enter' && currentGuess.length === NUMBER_LENGTH && turn <= MAX_TURNS) {
+            onEnter()
+            return
+        }
+        if (currentGuess.length >= NUMBER_LENGTH)
+            return
+
+        if (VALID_KEYS.includes(key)) {
+            onInput(key)
+            return
+        }
+    }
+
+    function onInput(digit: string) {
+        const newGuess = currentGuess.concat(digit)
+        setCurrentGuess(newGuess)
+    }
+
+    function onDelete() {
+        const newGuess: string[] = currentGuess.splice(0,currentGuess.length-1)
+        setCurrentGuess(newGuess)
+    }
+
+    function arrayEquals(a: string[], b: string[]) {
+        return Array.isArray(a) &&
+            Array.isArray(b) &&
+            a.length === b.length &&
+            a.every((val, index) => val === b[index]);
+    }
+
+    function onEnter() {
+        if (arrayEquals(currentGuess, answer)) {
+            setGuesses([...guesses, currentGuess])
+            setState(GameStatus.WON)
+            console.log("WON")
+            return
+        }
+        if (turn === MAX_TURNS) {
+            setGuesses([...guesses, currentGuess])
+            setState(GameStatus.LOST)
+            console.log("LOST")
+            return
+        }
+
+        setGuesses([...guesses, currentGuess])
+        setTurn(turn + 1)
+        setCurrentGuess([])
+    }
+
+    return (
+        <div className="main">
+            <Header/>
+            <ThemeSwitch/>
+            {state !== GameStatus.PLAYING ? <Modal type={state} completedWords={guesses} solution={answer}/> : null}
+            <div className="board">
+                {Array.from(Array(MAX_TURNS - turn)).map((_, index) => <EmptyRow key={index}/>)}
+                {guesses.map((value, index) => <UsedRow key={index} guess={value} answer={answer}/>)}
+                {state === GameStatus.PLAYING ? <ActiveRow guess={currentGuess}/> : null}
+            </div>
+        </div>
+
+    )
+}
